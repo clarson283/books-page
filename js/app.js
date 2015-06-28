51fc7f9d4d821edeb1c0d6f0dca1cf472e26a417
Callie <clarson283@gmail.com>
2015-06-27 22:11:07 -0700

    slideshow

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+
+var i = 0;
+
+var slides = new Array();
+
+slides[0] = "images/books-one.jpg";
+slides[1] = "images/books-two.jpg";
+slides[2] = "images/books-three.jpg";
+
+var slide_Length = slides.length - 1;
+
+function slideshow() {
+	document.image.src = slides[i];
+	if  (i < slide_Length) {
+		i++
+	} else {
+		i = 0;
+	};
+	setTimeout("slideshow()", 20000);
+}
+
+window.onload = slideshow;
+
+
+
